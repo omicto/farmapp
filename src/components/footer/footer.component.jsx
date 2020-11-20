8d7c596bd77b35d1d7bc304fe657fe4a02b00821
omicto <tevas.dlr@gmail.com>
2020-11-19 23:05:13 -0700

    implement material BottomNavigation

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,32 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHome,
-  faFlask,
-  faStore,
-  faShoppingCart,
-  faUserMd,
-} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Storefront from "@material-ui/icons/Storefront";
+import HomeIcon from "@material-ui/icons/Home";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
-const Footer = () => (
-  <nav className="footer">
-    <Link to="/">
-      <FontAwesomeIcon icon={faHome} />
-    </Link>
-    <Link to="/labs">
-      <FontAwesomeIcon icon={faFlask} />
-    </Link>
-    <Link to="/shop">
-      <FontAwesomeIcon icon={faStore} />
-    </Link>
-    <Link to="/cart">
-      <FontAwesomeIcon icon={faShoppingCart} />
-    </Link>
-    <Link to="/chat">
-      <FontAwesomeIcon icon={faUserMd} />
-    </Link>
-  </nav>
-);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserMd, faFlask, faUser } from "@fortawesome/free-solid-svg-icons";
+
+import { BottomNavigationAction, BottomNavigation } from "@material-ui/core";
+
+const Footer = () => {
+  const history = useHistory();
+  const [current, setCurrent] = useState("");
+  return (
+    <BottomNavigation
+      value={current}
+      onChange={(event, newPath) => {
+        setCurrent(newPath);
+        history.push(`/${newPath}`);
+      }}
+      showLabels
+    >
+      <BottomNavigationAction label="Inicio" value="" icon={<HomeIcon />} />
+      <BottomNavigationAction
+        label="Laboratorios"
+        value="labs"
+        icon={<FontAwesomeIcon icon={faFlask} />}
+      />
+      <BottomNavigationAction
+        label="Farmacias"
+        value="shop"
+        icon={<Storefront />}
+      />
+      <BottomNavigationAction
+        label="Carrito"
+        value="cart"
+        icon={<ShoppingCartIcon />}
+      />
+      <BottomNavigationAction
+        label="Ayuda"
+        value="chat"
+        icon={<FontAwesomeIcon icon={faUserMd} />}
+      />
+    </BottomNavigation>
+  );
+};
 
 export default Footer;
