ae681eaea73e096ea4aeac16736d767ac96338f3
curais <craisochoa22@gmail.com>
2020-11-20 13:38:06 -0700

    updated pipeline

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -4,7 +4,7 @@ echo "$SSH_PRIVATE_KEY" > ../asus-pubkey.pem
 sudo chmod 400 ../asus-pubkey.pem
 echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 ssh -i $SSH_KEY_PATH ec2-user@ec2-3-90-30-33.compute-1.amazonaws.com " 
-    docker login ghcr.io --username github-account < ${{ secrets.CR_PAT }}
+    docker login ghcr.io --username $REPO_OWNER < $CR_PAT
     docker pull ghcr.io/omicto/farmapp:latest
     docker run -p 80:80 --name farmaap -d ghcr.io/omicto/farmapp:latest
 
