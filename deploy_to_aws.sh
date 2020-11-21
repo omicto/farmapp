22c504cb6b5a562ed0d532b4c871ac12015a4fe1
curais <craisochoa22@gmail.com>
2020-11-20 18:42:56 -0700

    Fix deploy script'

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,7 +1,7 @@
 echo 'Starting the deployment...'
 mkdir -p ~/.ssh/
-echo "$SSH_PRIVATE_KEY" > ../asus-pubkey.pem
-sudo chmod 400 ../asus-pubkey.pem
+echo "$SSH_PRIVATE_KEY" > ssh_key/asus-pubkey.pem
+sudo chmod 400 ssh_key/asus-pubkey.pem
 echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
 ssh -i $SSH_KEY_PATH ec2-user@ec2-3-90-30-33.compute-1.amazonaws.com " 
     docker login ghcr.io --username $REPO_OWNER < $CR_PAT
