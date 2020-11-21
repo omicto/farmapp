ef12333ab063e6fb216c04b6e9c06c363be05612
Cristhoper <48818072+curais@users.noreply.github.com>
2020-11-20 21:22:53 -0700

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -3,7 +3,10 @@ mkdir -p ~/.ssh/
 echo "$SSH_PRIVATE_KEY" > ./asus-pubkey.pem
 sudo chmod 400 ./asus-pubkey.pem
 echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
-ssh -i ./asus-pubkey.pem ec2-user@ec2-3-90-30-33.compute-1.amazonaws.com  "docker login ghcr.io --username=$REPO_OWNER --password=$CR_PAT
+ssh -i ./asus-pubkey.pem ec2-user@ec2-3-90-30-33.compute-1.amazonaws.com  "
+    docker stop farmaap
+    docker rm farmaap
+    docker login ghcr.io --username=$REPO_OWNER --password=$CR_PAT
     docker pull ghcr.io/omicto/farmapp:latest
     docker run -p 80:80 --name farmaap -d ghcr.io/omicto/farmapp:latest"
     
