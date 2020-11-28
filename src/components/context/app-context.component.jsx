c557a58c00792d47001faa13dfbf313d03274bce
omicto <tevas.dlr@gmail.com>
2020-11-28 15:33:12 -0700

    Implement App Context

---
+++ src/components/context/app-context.component.jsx
@@ -0,0 +1,41 @@
+import React, { useReducer, createContext, useEffect } from "react";
+import PropTypes from "prop-types";
+
+const initialState = {
+  cart: [],
+};
+
+const persistedState = JSON.parse(sessionStorage.getItem("farmapp"));
+
+const finalInitialState = { ...initialState, ...persistedState };
+
+const appReducer = (state, action) => {
+  switch (action.type) {
+    case "ADDTOCART": {
+      return { ...state, cart: [...state.cart, action.payload] };
+    }
+    default:
+      return state;
+  }
+};
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => {
+  const [state, dispatch] = useReducer(appReducer, finalInitialState);
+
+  useEffect(() => {
+    sessionStorage.setItem("farmapp", JSON.stringify(state));
+    console.log(state);
+  }, [state]);
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+AppProvider.propTypes = {
+  children: PropTypes.element,
+};
